6196f8b51608234fd454d2c63b03e5ead7a8c3e6
CPFS\nvcryo <nvscanning.cpfs@gmail.com>
2025-05-16 15:38:58 +0200

    Created script to connect to ANC300 module

---
+++ ANC300/ANC300.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 16 13:33:32 2025
+
+@author: Lotte
+
+Connect to ANC300 via telnet
+
+commands:
+    getm/setm <Axis>: get/set the mode of the axis - gnd (disables all outputs), off (offset mode), more (see manual)
+    geta/seta <Axis>: get/set offset voltage on the axis (range between 0-3V at RT and 0-7.5V at LT)
+    echo <on/off>: turn console echo on/off
+    See manual for other commands
+
+"""
+
+import telnetlib
+import time
+import numpy as np
+
+class TelnetFunction:
+    def __init__(self, host, port):
+        self.connect(host, port)
+        
+    def connect(self, host, port): #connect to the machine
+        self.tn = telnetlib.Telnet(host, port)
+        self.tn.read_until(b"Authorization code: ")
+        self.tn.write(b"123456\n") #returns authorization code
+        self.tn.read_until(b"> ")
+        print('connected')
+        
+    def do(self, command, Print=True, sleeptime=0.05): #send a command to the machine
+        cmd = command + "\n" 
+        self.tn.write(cmd.encode('ascii')) #give command to console
+        time.sleep(sleeptime) #wait for console output
+        output = self.tn.read_very_eager().decode('utf-8') #read output from console
+        # for i, out in enumerate(output.split()):
+        #     print(f"{i}: {out}")
+            
+        if Print == True:
+            print(output) #print console output
+        
+        return output
+            
+    def ramp(self, axis, voltage):
+        V_0 = float(self.do(f"geta {axis}").split()[2])
+        print(V_0)
+        steps = np.arange(V_0, voltage, 0.01) + 0.01
+        print(steps)
+        for step in steps:
+            self.do(f"seta {axis} {step}", Print=False, sleeptime=0.01)
+            
+        self.do(f"geta {axis}")
+    
+    def close(self): #close connection with machine
+        self.tn.close()
+
+host = "192.168.10.2" #ANC 300 IP
+port = 7230 #standard console port
+
+tn = TelnetFunction(host, port) #connect to machine
+for i in range(3):
+    tn.do(f"setm {i+1} off") #set all axis to offset mode
+    
+tn.do("echo off")
+
+t1 = time.time()
+
+tn.ramp(1, 2)
+
+t2 = time.time()
+
+print(f"Ramp time: {t2 -t1}s, avg:{(t2-t1)/20}")
+
+tn.do("geta 1") #get offset voltage axis 1
+tn.do("seta 1 0.1") #set offset voltage axis 1
+tn.do("geta 1")
+tn.do("seta 1 0")
+tn.do("getm 3")
+
+tn.close()
+
+
+
+
